64c4480482d4f7c0a49cd113ef892a282ce2911e
Monishguru <monishguru@gmail.com>
2022-06-15 15:55:07 +0530

    Changes during class
    Changes during class

--- src/main/java/com/dhl/test/trackingservice/TrackingserviceApplication.java
+++ src/main/java/com/dhl/test/trackingservice/TrackingserviceApplication.java
@@ -2,8 +2,10 @@ package com.dhl.test.trackingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TrackingserviceApplication {
 
 	public static void main(String[] args) {
